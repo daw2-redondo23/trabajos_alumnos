ca8d81f5e4a4a813d6e3459bd6e25d8a1b53ea4b
daw2-redondo23 <113383170+daw2-redondo23@users.noreply.github.com>
2023-05-08 17:00:04 +0200

    imagenes

--- assets/homeVista-742f8785.js
+++ assets/homeVista-742f8785.js
@@ -1,7 +1,7 @@
 const homeVista = {
   template: `
     <div class="vh-100 d-flex flex-column align-items-center justify-content-center">
-      <img src="/assets/logo_vanilla.svg" class="w-25 d-block" alt="Vanilla Games">
+      <img src="/images/logo_vanilla.svg" class="w-25 d-block" alt="Vanilla Games">
       <h1>Vanilla Games</h1>
     </div>
     
