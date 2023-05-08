4828320ddd0c6f6b9bfd87e0baedb5c8baf73183
daw2-redondo23 <113383170+daw2-redondo23@users.noreply.github.com>
2023-05-08 17:29:48 +0200

    rutas actualizadas v2

--- assets/detalleProyectoVista-951c5727.js
+++ assets/detalleProyectoVista-951c5727.js
@@ -75,7 +75,7 @@ const detalleProyectoVista = {
       document.querySelector("#descripcion").value = proyectoActualizar.descripcion;
       document.querySelector("#enlace").value = proyectoActualizar.enlace;
       document.querySelector("#volver").addEventListener("click", (e) => {
-        window.location.href = "/#/proyectos";
+        window.location.href = "trabajos_alumnos/#/proyectos";
       });
     } catch (error) {
     }
